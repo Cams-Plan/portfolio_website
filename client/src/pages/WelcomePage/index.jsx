11c414b0fe4ba4d0f750c18af6977118dee1c4c9
Cams-Plan <ca.francis@outlook.com>
2023-10-24 00:46:55 +0100

    navigations working on the welcome page

--- client/src/pages/WelcomePage/index.jsx
+++ client/src/pages/WelcomePage/index.jsx
@@ -5,12 +5,15 @@ import toolsIcon from "../../assets/images/toolsIcon.png";
 import topSecretIcon from "../../assets/images/topSecretIcon.png";
 import citLogo from "../../assets/images/citLogo.png"
 
+import { Link, useNavigate } from 'react-router-dom';
 import ReactCurvedText from "react-curved-text";
 import {isMobile} from 'react-device-detect';
 
 import "../../assets/css/welcomePage.css"
 
+
 const WelcomePage = () => {
+  const navigate = useNavigate()
 
   const [icon, setIcon] = useState("")
   const [quadText, setQuadText] = useState([0, 0, 0, 0, 0, "null", true, 0])
@@ -29,6 +32,42 @@ const WelcomePage = () => {
 
   const handleBlur = (e) => {
     setIcon("")
+  }
+
+  const handleClick = (e) => {
+    const elAltText = e.target.alt
+    const altArray = elAltText.split(" ")
+    console.log(altArray)
+    switch (altArray[0]) {
+      case "home":
+        setTimeout(() => {
+          navigate("/camille")
+        },300)
+        
+        break;
+
+      case "projects":
+        setTimeout(() => {
+          navigate("/camille/projects")
+        },300)
+        
+        break;
+      
+      case "secret":
+        setTimeout(() => {
+          navigate("/camille")
+        },300)
+        break;
+      
+      case "camintech":
+        setTimeout(() => {
+          window.open("https://camintech.co.uk", "_blank")
+        },300)
+        break;
+        
+      default:
+        break;
+    }
   }
 
   const handleHover = (e, activeIcon) => {
@@ -50,7 +89,7 @@ const WelcomePage = () => {
         break;
     
       case "welcome-q4":
-        
+        setQuadText([97, 170, 90, 130, 68, "TECH BLOG", false, "22"])
         break;
     
       default:
@@ -62,13 +101,17 @@ const WelcomePage = () => {
     setQuadText([0,0,0,0,0,"", false, "0"])
   }
 
+  const handleImageClick = () => {
+    setIcon("all")
+  }
+
   return (
     <>
     <div style={{height: '100vh'}}>
       <div className='welcome-headshot-container'>
         <ReactCurvedText
           width={321}
-          height={321}
+          height={327}
           cx={quadText[0]}
           cy={quadText[1]}
           rx={quadText[2]}
@@ -77,7 +120,9 @@ const WelcomePage = () => {
           text={quadText[5]}
           reversed={quadText[6]}
           textProps={{ style: { fontSize: 17, transition: 'opacity 2s', fontFamily: 'Poppins', fontWeight: 400} }}
+          textPathProps={{fill: "#faebd7"}}
           tspanProps={{"dy": quadText[7]}}
+          svgProps={{className: "welcome-curved-text"}}
         />
         <div className='welcome-outer-circle'>
           <button className='welcome-q1' 
@@ -86,7 +131,11 @@ const WelcomePage = () => {
             onFocus={(e) => handleFocus(e)} 
             onBlur={(e) => handleBlur(e)} 
           >
-            {icon == "q1" ? <img className='welcome-icon-img' src={homeIcon} alt='house icon from flaticon by Kiranshastry'></img>: null}
+            {icon == "q1" || icon == "all" ? <img 
+            className='welcome-icon-img' 
+            src={homeIcon} 
+            onClick={(e) => handleClick(e)}
+            alt='home icon from flaticon by Kiranshastry'></img>: null}
           </button>
           
           <button className='welcome-q2'
@@ -95,7 +144,7 @@ const WelcomePage = () => {
             onFocus={(e) => handleFocus(e)} 
             onBlur={(e) => handleBlur(e)} 
           >
-            {icon == "q2" ? <img className='welcome-icon-img' src={toolsIcon} alt='house icon from flaticon'></img>: null}
+            {icon == "q2" || icon == "all" ? <img className='welcome-icon-img' src={toolsIcon} onClick={(e) => handleClick(e)} alt='projects icon from flaticon'></img>: null}
           </button>
 
           <button className='welcome-q3' 
@@ -104,20 +153,22 @@ const WelcomePage = () => {
             onFocus={(e) => handleFocus(e)} 
             onBlur={(e) => handleBlur(e)} 
           >
-            {icon == "q3" ? <img className='welcome-icon-img' src={topSecretIcon} alt='top-secret folder icon from flaticon by Good Ware'></img>: null}
+            {icon == "q3" ? <img className='welcome-icon-img' src={topSecretIcon} onClick={(e) => handleClick(e)} alt='secret folder icon from flaticon by Good Ware'></img>: null}
           </button>
           <button className='welcome-q4' 
-          onFocus={(e) => handleFocus(e)} 
-          onBlur={(e) => handleBlur(e)} >
-            {icon == "q4" ? <img className='welcome-icon-img' src={citLogo} alt='camintech blog logo'></img>: null}
+            onMouseEnter={(e) => handleHover(e, icon)} 
+            onMouseLeave={(e) => handleExit(e)}
+            onFocus={(e) => handleFocus(e)} 
+            onBlur={(e) => handleBlur(e)} 
+          >
+            {icon == "q4" ? <img className='welcome-icon-img' src={citLogo} onClick={(e) => handleClick(e)} alt='camintech blog logo'></img>: null}
           </button>
         </div>
         <img src={headshot} alt="camille's headshot" className='welcome-headshot' />
       </div>
       <div >
         <h1 className='welcome-header-text'><p className='welcome-header-part-1'>Camille Francis</p> <p className='welcome-header-part-2'>Full-Stack Cloud Developer</p></h1>
-      </div>
-      
+      </div>      
     </div>
     
     </>

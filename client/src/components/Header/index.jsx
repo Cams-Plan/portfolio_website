52d9966e01b0dde8d9a5ebf8a2970723175ccec7
Cams-Plan <ca.francis@outlook.com>
2023-10-24 19:44:41 +0100

    hamburger menu

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
+import styled from "styled-components"
+import "../../assets/css/navBarStyles.css"
+
+const NavBarContainer = styled.div
 
 const Header = () => {
+
+  const deviceSize = {
+    mobile: 768,
+    tablet: 992,
+    laptop: 1324,
+    desktop: 2024
+  }
+
+  const [sidebar, setSidebar] =useState()
+  const showSidebar = () => setSidebar(!sidebar)
+  
   return (
+   
     <>
-    <nav>
-      <NavLink to="/camille">Home</NavLink>
-      <NavLink to="/camille/projects">Projects</NavLink>
+    <div>
+      <img src={burgerBar} alt="" />
+    </div>
+    <nav className={sidebar ? 'nav-menu active': 'nav-menu'}>
+      <ul className='nav-menu-items'>
+        <li>
+          <NavLink to="/camille">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/camille/projects">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="">Tech Blog</NavLink>
+        </li>
+      </ul>
     </nav>
     <Outlet/>
     </>
